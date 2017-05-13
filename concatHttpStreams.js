cb2ed24db2f94bf66552b77e2ce0dcdc507ca780
hanabinoir <crigshn@gmail.com>
2017-05-12 19:10:18 -0700

    concat streams finished

--- concatHttpStreams.js
+++ concatHttpStreams.js
@@ -3,6 +3,8 @@ var concatStream = require('concat-stream');
 
 http.get(process.argv[2], function (response) {
     response.pipe(concatStream(function (data) {
-        console.log(data);
+        var str = data.toString();
+        console.log(str.length);
+        console.log(str);
     }));
 });
