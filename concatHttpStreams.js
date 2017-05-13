32c0ac6962d2ef9d71804c717f9bf2d0dd1b37b4
hanabinoir <crigshn@gmail.com>
2017-05-12 18:25:12 -0700

    working on concat-stream

---
+++ concatHttpStreams.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var concatStream = require('concat-stream');
+
+http.get(process.argv[2], function (response) {
+    response.pipe(concatStream(function (data) {
+        console.log(data);
+    }));
+});
